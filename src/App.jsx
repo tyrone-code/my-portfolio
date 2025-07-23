c2dabc48b89d1294c4b14e3984ffe8aa7763e7f9
Tyrone <tyronessglaw@gmail.com>
2025-07-23 12:09:58 +0200

    added about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import Navbar from "./my_components/Navbar";
+import About from "./my_components/About";
+
 function App() {
   return (
     <>
       <Navbar />
+      <About />
     </>
   );
 }
---
+++ src/my_components/About.jsx
@@ -0,0 +1,61 @@
+import myPhoto from "../images/picture.jpg";
+
+function About() {
+  return (
+    <>
+      <div className="about-section">
+        <div className="profile">
+          <img src={myPhoto} alt="App Logo" />
+          <div className="icon-links">
+            <a
+              href="https://www.linkedin.com/in/yourusername/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="mailto:someone@example.com">
+              <i className="fas fa-envelope"></i>
+            </a>
+          </div>
+        </div>
+        <div className="about-text">
+          <h1>Hi i'm Tyrone Schoeman</h1>
+          <p>
+            I am a self-taught front-end web developer based in Krugersdorp
+            Gauteng. I formally worked as a paralegal before switching careers
+            to front-end web development. My goal is looking to become fullstack
+            developer and then eventually specialize in backend development.
+          </p>
+          <p>
+            I have created three projects with foundational fundamental html,
+            css and javascript which you can see at the project section. The
+            current portfolio website you are currently viewing is made with the
+            react library.
+          </p>
+          <p>
+            During my free time, I enjoy gaming and watching sports,
+            additionally, I love camping and have a deep appreciation for
+            nature, often finding joy in hiking through its scenic beauty.
+          </p>
+          <p>
+            This website was crafted with the purpose of showcasing the projects
+            and experiences I've been fortunate to be a part of. Currently, I am
+            actively seeking new opportunities and connections, so please feel
+            free to get in touch with me!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;

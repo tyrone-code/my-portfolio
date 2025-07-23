a5cfc3b01872e616551b2e5a1956949dc714057a
Tyrone <tyronessglaw@gmail.com>
2025-07-23 10:49:13 +0200

    Added my project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Navbar from "./my_components/Navbar";
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/my_components/Navbar.jsx
@@ -0,0 +1,22 @@
+function Navbar() {
+  return (
+    <>
+      <ul className="navbar">
+        <a>
+          <li>About</li>
+        </a>
+        <a>
+          <li>Skills</li>
+        </a>
+        <a>
+          <li>Project</li>
+        </a>
+        <a>
+          <li>Contact</li>
+        </a>
+      </ul>
+    </>
+  );
+}
+
+export default Navbar;
